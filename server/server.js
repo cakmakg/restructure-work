35580ba994bd3018604fcb06df317d33f11df1ab
cakmakg <cakmak4834@gmail.com>
2026-02-10 00:17:31 +0100

    server.js up

--- server/server.js
+++ server/server.js
@@ -259,4 +259,6 @@ app.post('/api/rag-chat', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🎧 Server läuft auf Port ${PORT}...`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server ${PORT} portunda çalışıyor...`);
+});
